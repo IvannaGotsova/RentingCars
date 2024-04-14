52ab9165a23e31ae0970bdd584fd8317ef735c5c
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-14 15:13:52 +0300

    Initial commit -  basics

--- RentingCars/Program.cs
+++ RentingCars/Program.cs
@@ -43,7 +43,7 @@ namespace RentingCars
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Index}/{id?}");     
             app.MapRazorPages();
 
             app.Run();
