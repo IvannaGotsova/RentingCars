c2040dd38fe12cc8d7fc00080944aacd17d1156c
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-15 17:35:15 +0300

    Modified DBContext wth some examples of data

--- RentingCars/Data/RentingCarsDbContext.cs
+++ RentingCars/Data/RentingCarsDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using RentingCars.Data.Entities;
 
 namespace RentingCars.Data
 {
-    public class RentingCarsDbContext : IdentityDbContext
+    public class RentingCarsDbContext : IdentityDbContext<ApplicationUser>
     {
         public RentingCarsDbContext(DbContextOptions<RentingCarsDbContext> options)
             : base(options)
@@ -16,7 +17,14 @@ namespace RentingCars.Data
         public DbSet<Car> Cars { get; init; }
         public DbSet<Type> Types { get; init; }
 
-
+        private IdentityUser BrokerUser { get; set; }
+        private IdentityUser DemoUser { get; set; }
+        private Type Family { get; set; }
+        private Type Standard { get; set; }
+        private Type Luxury { get; set; }
+        private Car FamilyCar { get; set; }
+        private Car StandardCar { get; set; }
+        private Car LuxuryCar { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder
