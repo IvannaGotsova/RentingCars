059042615003da19eb2a3d7c572ad460b0efbb46
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-15 18:49:44 +0300

    Seed data

--- RentingCars/Data/RentingCarsDbContext.cs
+++ RentingCars/Data/RentingCarsDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using RentingCars.Data.Entities;
+using System.Net.Sockets;
 
 namespace RentingCars.Data
 {
@@ -13,15 +14,15 @@ namespace RentingCars.Data
             this.Database.Migrate();
         }
 
-        public DbSet<Broker> Brokers {  get; init; }
+        public DbSet<Broker> Brokers { get; init; }
         public DbSet<Car> Cars { get; init; }
-        public DbSet<Type> Types { get; init; }
+        public DbSet<Entities.Type> Types { get; init; }
 
         private IdentityUser BrokerUser { get; set; }
         private IdentityUser DemoUser { get; set; }
-        private Type Family { get; set; }
-        private Type Standard { get; set; }
-        private Type Luxury { get; set; }
+        private Entities.Type Family { get; set; }
+        private Entities.Type Standard { get; set; }
+        private Entities.Type Luxury { get; set; }
         private Car FamilyCar { get; set; }
         private Car StandardCar { get; set; }
         private Car LuxuryCar { get; set; }
@@ -42,9 +43,99 @@ namespace RentingCars.Data
                 .HasForeignKey(c => c.BrokerId)
                 .OnDelete(DeleteBehavior.Restrict);
 
+            SeedUsers();
+            builder
+                .Entity<ApplicationUser>()
+                .HasData(this.BrokerUser, this.DemoUser);
+
+            SeedBrokers();
+            builder
+                .Entity<Broker>()
+                .HasData(this.Broker);
+
+            SeedTypes();
+            builder
+                .Entity<Entities.Type>()
+                .HasData(this.Family, this.Standard, this.Luxury);
+
+            SeedCars();
+            builder
+                .Entity<Car>()
+                .HasData(this.FamilyCar, this.StandardCar, this.LuxuryCar);
+
 
 
             base.OnModelCreating(builder);
+        }
+
+        private void SeedUsers()
+        {
+            var hasher = new PasswordHasher<IdentityUser>();
+
+            this.BrokerUser = new IdentityUser()
+            {
+                Id = "dea12856-c198-4129-b3f3-b893d8395082",
+                UserName = "brokeruser@example.com",
+                NormalizedUserName = "brokeruser@example.com",
+                Email = "brokeruser@example.com",
+                NormalizedEmail = "brokeruser@example.com",
+            };
+
+            this.BrokerUser.PasswordHash =
+                 hasher.HashPassword(this.BrokerUser, "brokeruser123");
+
+            this.DemoUser = new IdentityUser()
+            {
+                Id = "6d5800ce-d726-4fc8-83d9-d6b3ac1f591e",
+                UserName = "demouser@example.com",
+                NormalizedUserName = "demouser@example.com",
+                Email = "demouser@example.com",
+                NormalizedEmail = "demouser@example.com",
+            };
+
+            this.DemoUser.PasswordHash =
+            hasher.HashPassword(this.DemoUser, "demouser123");
+        }
+
+        private void SeedBrokers()
+        {
+            this.Broker = new Broker()
+            {
+                Id = 1,
+                BrokerPhoneNumber = "+0000000000",
+                UserId = this.BrokerUser.Id
+            };
+        }
+
+        private void SeedTypes()
+        {
+            this.Family = new Entities.Type()
+            {
+                Id = 1,
+                TypeName = "Family"
+            };
+
+            this.Standard = new Entities.Type()
+            {
+                Id = 1,
+                TypeName = "Standard"
+            };
+
+            this.Luxury = new Entities.Type()
+            {
+                Id = 1,
+                TypeName = "Luxury"
+            };
+
+        }
+
+        private void SeedCars()
+        {
+            this.FamilyCar = new Car()
+            {
+                Id = 1,
+
+            };
         }
     }
 }
