4ad90e815ccb3a3a37089a4aa371468877ce81ab
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-15 22:11:15 +0300

    Modified builder

--- RentingCars/Data/RentingCarsDbContext.cs
+++ RentingCars/Data/RentingCarsDbContext.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using RentingCars.Data.Entities;
-using System.Net.Sockets;
 
 namespace RentingCars.Data
 {
@@ -14,21 +13,22 @@ namespace RentingCars.Data
             this.Database.Migrate();
         }
 
-        public DbSet<Broker> Brokers { get; init; }
-        public DbSet<Car> Cars { get; init; }
-        public DbSet<Entities.Type> Types { get; init; }
+        public DbSet<Broker>? Brokers { get; init; } = null!;
+        public DbSet<Car>? Cars { get; init; } = null!;
+        public DbSet<Entities.Type>? Types { get; init; } = null!;
 
-        private IdentityUser BrokerUser { get; set; }
-        private IdentityUser DemoUser { get; set; }
-        private Entities.Type Family { get; set; }
-        private Entities.Type Standard { get; set; }
+        private IdentityUser BrokerUser { get; set; } = null!;
+        private IdentityUser DemoUser { get; set; } = null!;
+        private Entities.Type Family { get; set; } = null!;
+        private Entities.Type Standard { get; set; } = null!;
         private Entities.Type Luxury { get; set; }
-        private Car FamilyCar { get; set; }
-        private Car StandardCar { get; set; }
-        private Car LuxuryCar { get; set; }
-        private Broker Broker { get; set; }
+        private Car FamilyCar { get; set; } = null!;
+        private Car StandardCar { get; set; } = null!;
+        private Car LuxuryCar { get; set; } = null!;
+        private Broker Broker { get; set; } = null!;
         protected override void OnModelCreating(ModelBuilder builder)
         {
+          
             builder
                 .Entity<Car>()
                 .HasOne(c => c.Type)
@@ -45,7 +45,7 @@ namespace RentingCars.Data
 
             SeedUsers();
             builder
-                .Entity<ApplicationUser>()
+                .Entity<IdentityUser>()
                 .HasData(this.BrokerUser, this.DemoUser);
 
             SeedBrokers();
@@ -63,7 +63,29 @@ namespace RentingCars.Data
                 .Entity<Car>()
                 .HasData(this.FamilyCar, this.StandardCar, this.LuxuryCar);
 
+            builder
+                .Entity<ApplicationUser>()
+                .Property(u => u.UserName)
+                .HasMaxLength(20)
+                .IsRequired();
 
+            builder
+                .Entity<ApplicationUser>()
+                .Property(u => u.Email)
+                .HasMaxLength(60)
+                .IsRequired();
+
+            builder
+               .Entity<ApplicationUser>()
+               .Property(u => u.FirstName)
+               .HasMaxLength(60)
+               .IsRequired();
+
+            builder
+               .Entity<ApplicationUser>()
+               .Property(u => u.LastName)
+               .HasMaxLength(60)
+               .IsRequired();
 
             base.OnModelCreating(builder);
         }
