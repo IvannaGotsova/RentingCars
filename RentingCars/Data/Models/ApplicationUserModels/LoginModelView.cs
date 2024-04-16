e284dff5fbe70d04f0121e9608ee571746b2c44b
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-16 22:28:14 +0300

    Added LoginModelView

---
+++ RentingCars/Data/Models/ApplicationUserModels/LoginModelView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static RentingCars.Data.DataConstants.ApplicationUserConstants;
+
+namespace RentingCars.Data.Models.ApplicationUserModels
+{
+    public class LoginModelView 
+    {
+        [Required]
+        [StringLength(AppUserUserNameMaxLength, MinimumLength = AppUserUserNameMinLength)]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
