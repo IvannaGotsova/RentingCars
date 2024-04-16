585e7484fc8c79cd931701ebcf2e5455bc71b65c
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-16 23:11:25 +0300

    Created CarsController

---
+++ RentingCars/Controllers/CarsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentingCars.Controllers
+{
+    public class CarsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
