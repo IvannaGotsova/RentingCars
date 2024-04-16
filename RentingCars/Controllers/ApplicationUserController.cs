faed3b762e76aed21d2cde19ec2b78fedf79d320
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-16 22:35:02 +0300

    Added Controler for ApplicationUSer

---
+++ RentingCars/Controllers/ApplicationUserController.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RentingCars.Data.Entities;
+using RentingCars.Data.Models.ApplicationUserModels;
+
+
+namespace RentingCars.Controllers
+{
+    public class ApplicationUserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public ApplicationUserController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            RegisterModelView modelToBeRegistered = new();
+
+            TempData["message"] = $"Hello! Welcome!";
+
+            return View(modelToBeRegistered);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterModelView modelToBeRegistered)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(modelToBeRegistered);
+            }
+
+            ApplicationUser userToBeRegistered = new()
+            {
+                UserName = modelToBeRegistered.UserName,
+                Email = modelToBeRegistered.Email,
+                FirstName = modelToBeRegistered.FirstName,
+                LastName = modelToBeRegistered.LastName
+            };
+
+            var resultUserToBeRegistered = await userManager
+                .CreateAsync(userToBeRegistered, modelToBeRegistered.Password);
+            
+            if (!resultUserToBeRegistered.Succeeded)
+            {
+                foreach (var error in resultUserToBeRegistered.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(modelToBeRegistered);
+            }
+
+            return RedirectToAction("Login", "ApplicationUsers");
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+
+            if (User.IsInRole("Admin"))
+            {
+                return RedirectToAction("Index", "Home", new { area = "Admin" });
+            }
+
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            LoginModelView modelToBeLogin = new();
+
+            TempData["message"] = $"Hello! Have a great time!";
+
+            return View(modelToBeLogin);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginModelView modelToBeLogin)
+        {   
+            if (!ModelState.IsValid)
+            {
+                return View(modelToBeLogin);
+            }
+
+            var userToBeLogin = await userManager
+                .FindByNameAsync(modelToBeLogin.UserName);
+
+            if (userToBeLogin != null)
+            {
+                var resultUserToBeLogin = await signInManager
+                    .PasswordSignInAsync(userToBeLogin, modelToBeLogin.Password, true, false);
+
+                if (resultUserToBeLogin.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+            }
+
+            ModelState.AddModelError("", "Invalid login attempt.");
+
+            return View(modelToBeLogin);
+
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+
+            TempData["message"] = $"Goodbye! We are waiting for you to come back";
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
