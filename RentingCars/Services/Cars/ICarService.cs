b0baeab1d8ea107db50ccda62d51521b0f20c2aa
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-17 17:49:28 +0300

    Added ICarService wih first method

---
+++ RentingCars/Services/Cars/ICarService.cs
@@ -0,0 +1,9 @@
+﻿using RentingCars.Services.Models.Cars;
+
+namespace RentingCars.Services.Car
+{
+    public interface ICarService
+    {
+        IEnumerable<CarIndexServiceModel> LastThreeCars();
+    }
+}
