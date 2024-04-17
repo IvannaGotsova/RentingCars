ae647655564f2ea4c42156b72494a914750c0a9f
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-17 21:59:37 +0300

    Added new methods in the Service

--- RentingCars/Services/Cars/CarService.cs
+++ RentingCars/Services/Cars/CarService.cs
@@ -1,7 +1,10 @@
 ﻿using RentingCars.Data;
+using RentingCars.Data.Entities;
+using RentingCars.Data.Models.Car;
 using RentingCars.Services.Models.Cars;
+using System.Reflection.Metadata.Ecma335;
 
-namespace RentingCars.Services.Car
+namespace RentingCars.Services.Cars
 {
     public class CarService : ICarService
     {
@@ -10,6 +13,18 @@ namespace RentingCars.Services.Car
         public CarService(RentingCarsDbContext rentingCarsDbContextdata)
         {
             this.rentingCarsDbContextdata = rentingCarsDbContextdata;
+        }
+
+        public IEnumerable<CarTypeServiceModel> AllCarsTypes()
+        {
+            return
+                this.rentingCarsDbContextdata
+                .Types
+                .Select(t => new CarTypeServiceModel
+                {
+                    Id = t.Id,
+                    TypeName = t.TypeName
+                }).ToList();
         }
 
         public IEnumerable<CarIndexServiceModel> LastThreeCars()
@@ -25,7 +40,35 @@ namespace RentingCars.Services.Car
                     CarImageUrl = c.CarImageUrl
                 })
                 .Take(3);
-
         }
+
+        public bool TypeExists(int typeId)
+        {
+            return
+                this.rentingCarsDbContextdata
+                .Types
+                .Any(t => t.Id == typeId);
+        }
+
+        public int CreateCar(string CarBrand, string CarModel, string CarDescription, string CarAdditionalInformation, string CarImageURL, decimal CarPricePerDay, int CarTypeId, int CarBrokerId)
+        {
+            var car = new Car
+            {
+                CarBrand = CarBrand,
+                CarModel = CarModel,
+                CarDescription = CarDescription,
+                CarAdditionalInformation = CarAdditionalInformation,
+                CarImageUrl = CarImageURL,
+                CarPricePerDay = CarPricePerDay,
+                TypeId = CarTypeId,
+                BrokerId = CarBrokerId
+            };
+
+            this.rentingCarsDbContextdata.Cars.Add(car);
+            this.rentingCarsDbContextdata.SaveChanges();
+
+            return car.Id;
+        }
+
     }
 }
--- RentingCars/Services/Cars/ICarService.cs
+++ RentingCars/Services/Cars/ICarService.cs
@@ -1,9 +1,12 @@
 ﻿using RentingCars.Services.Models.Cars;
 
-namespace RentingCars.Services.Car
+namespace RentingCars.Services.Cars
 {
     public interface ICarService
     {
         IEnumerable<CarIndexServiceModel> LastThreeCars();
+        IEnumerable<CarTypeServiceModel> AllCarsTypes();
+        bool TypeExists(int typeId);
+        int CreateCar(string CarBrand, string CarModel, string Description, string AdditionalInformation, string ImageURL, decimal PricePerDay, int TypeId, int BrokerId);
     }
 }
