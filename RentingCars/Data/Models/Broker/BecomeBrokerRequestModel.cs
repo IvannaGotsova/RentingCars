a9778856a9046bbd24a789c1c6b284bc0f9689d2
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-17 21:58:48 +0300

    Created model

--- RentingCars/Data/Models/Broker/BecomeBrokerRequestModel.cs
+++ RentingCars/Data/Models/Broker/BecomeBrokerRequestModel.cs
@@ -1,6 +1,14 @@
-﻿namespace RentingCars.Data.Models.Broker
+﻿using System.ComponentModel.DataAnnotations;
+using static RentingCars.Data.DataConstants.BrokerConstants;
+
+namespace RentingCars.Data.Models.Broker
 {
     public class BecomeBrokerRequestModel
     {
+        [Required]
+        [StringLength(BrokerPhoneNumberMaxLength, MinimumLength =BrokerPhoneNumberMinLength)]
+        [Display(Name = "Phone Number")]
+        [Phone]
+        public string BrokerPhoneNumber { get; init; }
     }
 }
