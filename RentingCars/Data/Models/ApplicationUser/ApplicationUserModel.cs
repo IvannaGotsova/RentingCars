7a915ef84b0ef4f1d25d3ea08a120cffd5c3d791
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-17 17:14:04 +0300

    Added model

---
+++ RentingCars/Data/Models/ApplicationUser/ApplicationUserModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static RentingCars.Data.DataConstants.ApplicationUserConstants;
+
+namespace RentingCars.Data.Models.ApplicationUserModels
+{
+
+    public class ApplicationUserModel
+    {
+        public string Id { get; set; } = null!;
+        [Required]
+        [StringLength(AppUserUserNameMaxLength, MinimumLength = AppUserUserNameMinLength)]
+        public string UserName { get; set; } = null!;
+        [Required]
+        [StringLength(AppUserFirstNameMaxLength, MinimumLength = AppUserFirstNameMinLength)]
+        public string? FirstName { get; set; }
+        [Required]
+        [StringLength(AppUserLastNameMaxLength, MinimumLength = AppUserLastNameMinLength)]
+        public string? LastName { get; set; }
+    }
+}
