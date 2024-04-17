8a704637002b01626c679c94166b34a19aba10b5
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-17 18:45:36 +0300

    Added ICarservice

--- RentingCars/Controllers/HomeController.cs
+++ RentingCars/Controllers/HomeController.cs
@@ -1,16 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using RentingCars.Data.Models.Home;
 using RentingCars.Models;
+using RentingCars.Services.Car;
 using System.Diagnostics;
 
 namespace RentingCars.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ICarService carService;
+
+        public HomeController(ICarService carService)
+        {
+            this.carService = carService;
+        }
 
         public IActionResult Index()
         {
-            return View(new IndexViewModel());
+            var cars = this.carService.LastThreeCars();
+            return View(cars);
         }
 
         public IActionResult Privacy()
