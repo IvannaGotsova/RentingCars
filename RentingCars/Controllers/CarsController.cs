05e54a6f1f804336411834db9c859dc0deabe6a3
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-17 17:12:51 +0300

    Added actions to CarsController

--- RentingCars/Controllers/CarsController.cs
+++ RentingCars/Controllers/CarsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RentingCars.Data.Models.Car;
 
 namespace RentingCars.Controllers
 {
@@ -8,5 +10,69 @@ namespace RentingCars.Controllers
         {
             return View();
         }
+
+        public IActionResult All()
+        {
+            return View(new AllCarsRequestModel());
+        }
+
+        [Authorize]
+        public IActionResult Mine()
+        {
+            return View(new AllCarsRequestModel());
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(new CarDetailsRequestModel());
+        }
+
+        [Authorize]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Add(AddEditCarRequestModel addCarRequestModel)
+        {
+            return RedirectToAction(nameof(Details), new { id = "1"});
+        }
+
+        [Authorize]
+        public IActionResult Edit(int id)
+        {
+            return View(new AddEditCarRequestModel());
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Edit(int id, AddEditCarRequestModel editCarRequestModel)
+        {
+            return RedirectToAction(nameof(Details), new { id = "1" });
+        }
+
+        [Authorize]
+        public IActionResult Delete(int id)
+        {
+            return View(new CarDetailsRequestModel());
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Rent(int id)
+        {
+            return RedirectToAction(nameof(Mine));
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Return(int id)
+        {
+            return RedirectToAction(nameof(Mine));
+        }
+
+
     }
 }
