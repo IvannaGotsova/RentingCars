ee55b85f8d5bce4c15b8c96284561048bcbd2aa4
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-17 17:49:43 +0300

    Added model for Services

---
+++ RentingCars/Services/Models/Cars/CarIndexServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RentingCars.Services.Models.Cars
+{
+    public class CarIndexServiceModel
+    {
+        public int Id { get; set; }
+        public string? CarBrand { get; set; }
+        public string? CarImageUrl { get; set; }
+    }
+}
