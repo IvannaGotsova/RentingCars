f0fafc4276208608c3691c14dbbc07611cba00b9
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-17 21:58:15 +0300

    Added new actions

--- RentingCars/Controllers/BrokersController.cs
+++ RentingCars/Controllers/BrokersController.cs
@@ -1,18 +1,31 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using static RentingCars.Common.ClaimsPrincipalExtensions;
 using RentingCars.Data.Models.Broker;
+using RentingCars.Services.Brokers;
 
 namespace RentingCars.Controllers
 {
     public class BrokersController : Controller
     {
+        private readonly IBrokerService brokerService;
+
+        public BrokersController(IBrokerService brokerService)
+        {
+            this.brokerService = brokerService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-
+        [Authorize]
         public IActionResult Become()
         {
+            if (this.brokerService.ExistById(this.User.Id()))
+            {
+                return BadRequest();
+            }
             return View();
         }
 
@@ -20,6 +33,31 @@ namespace RentingCars.Controllers
         [HttpPost]
         public IActionResult Become(BecomeBrokerRequestModel becomeBrokerRequestModel)
         {
+            var userId = this.User.Id();
+
+            if (this.brokerService.ExistById(userId))
+            {
+                return BadRequest();
+            }
+            return View();
+
+            if (this.brokerService.UserWithPhoneNumberExists(becomeBrokerRequestModel.BrokerPhoneNumber))
+            {
+                ModelState.AddModelError(nameof(becomeBrokerRequestModel.BrokerPhoneNumber), "There is already a broker with this phone number. Try again.");
+            }
+
+            if (this.brokerService.UserHasCarRents(userId))
+            {
+                ModelState.AddModelError("Error", "You must have cars in order to become broker!");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(becomeBrokerRequestModel);
+            }
+
+            this.brokerService.Create(userId, becomeBrokerRequestModel.BrokerPhoneNumber);
+
             return RedirectToAction(nameof(CarsController.All), "Cars");
         }
     }
