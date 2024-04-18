d097e119b40519941f919b473afae3e009eb7c8a
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-18 11:27:12 +0300

    Added model

---
+++ RentingCars/Services/Models/Cars/CarRequestServiceModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RentingCars.Services.Models.Cars
+{
+    public class CarRequestServiceModel
+    {
+        public int TotalCountCars { get; set; }
+
+        public IEnumerable<CarServiceModel> Cars 
+            = new List<CarServiceModel>();
+    }
+}
