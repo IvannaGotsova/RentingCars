db62dd7ea3fa25e348cec7f7da7a19fac62cfc94
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-18 11:27:20 +0300

    Added model

---
+++ RentingCars/Services/Models/Cars/CarServiceModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentingCars.Services.Models.Cars
+{
+    public class CarServiceModel
+    {
+        public int Id { get; init; }
+        [Display(Name = "Car Brand")]
+        public string? CarBrand { get; init; }
+        [Display(Name = "Car Model")]
+        public string? CarModel { get; init; }
+        [Display(Name = "Car Description")]
+        public string? CarDescription { get; init; }
+        [Display(Name = "Car Additional Information")]
+        public string? CarAdditionalInformation { get; init; }
+        [Display(Name = "Car Image URL")]
+        public string? CarImageUrl { get; init; }
+        [Display(Name = "Car Price Per Day")]
+        public decimal CarPricePerDay { get; init; }
+        [Display(Name = "Is Car Rented")]
+        public bool isRented { get; init; }
+
+    }
+}
