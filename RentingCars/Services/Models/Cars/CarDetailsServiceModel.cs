ee0d49adef7a37b2c0962d84013d6c91016b484c
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-18 12:51:53 +0300

    Added model

---
+++ RentingCars/Services/Models/Cars/CarDetailsServiceModel.cs
@@ -0,0 +1,11 @@
+﻿using RentingCars.Data.Models.Broker;
+
+namespace RentingCars.Services.Models.Cars
+{
+    public class CarDetailsServiceModel : CarServiceModel
+    {
+        public string? TypeName { get; init; }
+
+        public BrokerServiceModel Broker { get; init; }
+    }
+}
