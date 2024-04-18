e6c34f49838f330d5a0e524416c354b8fefe22a4
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-18 12:51:32 +0300

    Added model

---
+++ RentingCars/Data/Models/Broker/BrokerServiceModel.cs
@@ -0,0 +1,11 @@
+﻿using RentingCars.Data.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentingCars.Data.Models.Broker
+{
+    public class BrokerServiceModel
+    {
+        public string? BrokerPhoneNumber { get; set; }
+        public string? BrokerEmail { get; set; }
+    }
+}
