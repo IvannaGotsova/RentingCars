a9caaf6539fa059bca1e1c72c33e55211fbaada9
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-22 17:38:42 +0300

    Created for protection of URL

---
+++ RentingCars/Services/Models/Cars/ICarModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RentingCars.Services.Models.Cars
+{
+    public interface ICarModel
+    {
+        string CarBrand { get; }
+        string CarModel { get; }
+        string CarDescription { get; }
+        string CarAdditionalInformation { get; }
+
+    }
+}
