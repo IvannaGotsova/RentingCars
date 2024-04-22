9339486b42e15bada56c5e3e26638c53acc9d06a
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-22 17:37:23 +0300

    Created to modify URL

---
+++ RentingCars/Common/ModelExtensions.cs
@@ -0,0 +1,27 @@
+﻿using RentingCars.Services.Models.Cars;
+using System.Text.RegularExpressions;
+
+namespace RentingCars.Common
+{
+    public static class ModelExtensions
+    {
+        public static string GetInformation(this ICarModel carModel)
+        {
+            return carModel.CarBrand.Replace(" ", "-") + "-" + GetDescription(carModel.CarDescription) + GetAdditionalInformation(carModel.CarAdditionalInformation);
+        }
+
+        public static string GetDescription(string carDescription)
+        {
+            carDescription = string.Join("-", carDescription.Split(" ").Take(3));
+
+            return carDescription;
+        }
+
+        public static string GetAdditionalInformation(string carAdditionalInformation)
+        {
+            carAdditionalInformation = string.Join("-", carAdditionalInformation.Split(" ").Take(3));
+
+            return carAdditionalInformation;
+        }
+    }
+}
