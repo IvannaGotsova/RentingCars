bea60e31b275bc4a5edc83875a7017238b367719
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-23 20:53:10 +0300

    Revert "Revert "Added Constants for Admin""
    This reverts commit 17d924e0b5913384e408b72db54db3c3f37fdf67.

---
+++ RentingCars/AdminConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace RentingCars
+{
+    public class AdminConstants
+    {
+        public const string AdminRoleName = "Administrator";
+        public const string AdminEmail = "admin@mail.com";
+    }
+}
