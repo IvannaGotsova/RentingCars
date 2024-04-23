2bbe015aad5c5f894b9c30a063377b1638317979
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-23 20:13:47 +0300

    Created ApplicationUser Service and interface

---
+++ RentingCars/Services/ApplicationUsers/ApplicationUserService.cs
@@ -0,0 +1,30 @@
+﻿using RentingCars.Data;
+
+namespace RentingCars.Services.ApplicationUsers
+{
+    public class ApplicationUserService : IApplicationUserService
+    {
+        private readonly RentingCarsDbContext rentingCarsDbContextData;
+
+        public ApplicationUserService(RentingCarsDbContext rentingCarsDbContextData)
+        {
+            this.rentingCarsDbContextData = rentingCarsDbContextData;
+        }
+
+        public string ApplicationUserFullName(string userId)
+        {
+            var applicationser =
+                this.rentingCarsDbContextData
+                .ApplicationUsers
+                .Find(userId);
+
+            if (string.IsNullOrEmpty(applicationser.FirstName)
+                || string.IsNullOrEmpty(applicationser.LastName))
+            {
+                return null;
+            }
+
+            return applicationser.FirstName + " " + applicationser.LastName;
+        }
+    }
+}
---
+++ RentingCars/Services/ApplicationUsers/IApplicationUserService.cs
@@ -0,0 +1,7 @@
+﻿namespace RentingCars.Services.ApplicationUsers
+{
+    public interface IApplicationUserService
+    {
+        string ApplicationUserFullName(string userId);
+    }
+}
