3b7973501e38a020c4ff12cae7ecffde654d4374
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-25 18:55:57 +0300

    Created new Area

--- RentingCars/AdminConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RentingCars
-{
-    public class AdminConstants
-    {
-        public const string AdminRoleName = "Administrator";
-        public const string AdminEmail = "admin@mail.com";
-    }
-}
