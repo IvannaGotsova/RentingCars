dd34d06d0841a6bc0eabc26ed94b9b1f5f4b60a7
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-25 20:16:53 +0300

    Change Index view if user is admin

--- RentingCars/Controllers/HomeController.cs
+++ RentingCars/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Diagnostics;
 
 namespace RentingCars.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller                
     {
         private readonly ICarService carService;
 
