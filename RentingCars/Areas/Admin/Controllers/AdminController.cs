33c23983f82c98efb018470839b66ca9bc56f4b4
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-25 19:01:24 +0300

    Created AdminController

---
+++ RentingCars/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static RentingCars.Areas.Admin.AdminConstants;
+
+namespace RentingCars.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Area(AreaName)]
+        [Authorize(Roles = AdminRoleName)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
