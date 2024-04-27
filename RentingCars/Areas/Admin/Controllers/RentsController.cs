8f3d41daa8f2d613615056b20a3d7846651f0966
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-27 12:15:25 +0300

    Created RentsController

---
+++ RentingCars/Areas/Admin/Controllers/RentsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RentingCars.Core.Services.Rents;
+using static RentingCars.Areas.Admin.AdminConstants;
+
+namespace RentingCars.Areas.Admin.Controllers
+{
+    [Area(AreaName)]
+    [Authorize(Roles = AdminRoleName)]
+    public class RentsController : AdminController
+    {
+        private readonly IRentService rentService;
+
+        public RentsController(IRentService rentService)
+        {
+            this.rentService = rentService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Rents/All")]
+        public IActionResult All()
+        {
+            var rents =
+                this.rentService
+                .AllRents();
+
+            return View(rents);
+        }
+    }
+}
