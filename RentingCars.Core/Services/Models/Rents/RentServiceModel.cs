480040222f6d7025b49141199263b519f0c8bc55
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-27 12:16:22 +0300

    Added model

---
+++ RentingCars.Core/Services/Models/Rents/RentServiceModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentingCars.Core.Services.Models.Rents
+{
+    public class RentServiceModel
+    {
+        public string CarBrand { get; init; }
+        public string CarModel { get; init; }
+        public string CarImageUrl { get; init; }
+        public decimal CarPricePerDay{ get; init; }
+        public string BrokerFullName { get; init; }
+        public string BrokerEmail { get; init; }
+        public string RenterFullName { get; init; }
+        public string RenterEmail { get; init; }
+    }
+}
