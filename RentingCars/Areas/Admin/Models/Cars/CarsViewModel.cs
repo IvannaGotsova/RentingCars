9bb66ec80429e82656f9bdd591625c5b7f9073bf
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-27 10:11:43 +0300

    Create CarsViewModel

---
+++ RentingCars/Areas/Admin/Models/Cars/CarsViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RentingCars.Areas.Admin.Models.Cars
+{
+    public class CarsViewModel
+    {
+        public IEnumerable<CarsViewModel> AddedCars { get; set; }
+        = new List<CarsViewModel>();
+
+        public IEnumerable<CarsViewModel> RentedCars { get; set; }
+        = new List<CarsViewModel>();
+    }
+}
