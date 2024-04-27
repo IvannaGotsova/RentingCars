dab08b29083ad2300161b2e5140178615f6cc9d1
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-27 11:38:46 +0300

    Added ApplicationUserServiceModel

---
+++ RentingCars.Core/Services/Models/ApplicationUser/ApplicationUserServiceModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentingCars.Core.Services.Models.ApplicationUser
+{
+    public class ApplicationUserServiceModel
+    {
+        public string ApplicationUserEmail { get; init; }
+        public string ApplicationUserFullName { get; init; }
+        public string ApplicationUserPhoneNumber { get; init; }
+    }
+}
