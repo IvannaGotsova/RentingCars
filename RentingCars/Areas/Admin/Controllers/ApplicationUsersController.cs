1e418c42b51fb5e16c6a933bc704b3bfdb4a9461
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-27 11:37:13 +0300

    Added All Action

---
+++ RentingCars/Areas/Admin/Controllers/ApplicationUsersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RentingCars.Core.Services.ApplicationUsers;
+using static RentingCars.Areas.Admin.AdminConstants;
+
+namespace RentingCars.Areas.Admin.Controllers
+{
+    [Area(AreaName)]
+    [Authorize(Roles = AdminRoleName)]
+    public class ApplicationUsersController : Controller
+    {
+        private readonly IApplicationUserService applicationUserService;
+
+        public ApplicationUsersController(IApplicationUserService applicationUserService)
+        {
+            this.applicationUserService = applicationUserService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Users/All")]
+        public IActionResult All()
+        {
+            var allUsers =
+                this.applicationUserService
+                .AllApplicationUsers();
+
+            return View(allUsers);
+        }
+    }
+}
