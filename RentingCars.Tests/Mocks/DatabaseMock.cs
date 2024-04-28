213bbe69044f8199a50eeabd87c22a6b5ff83d77
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-28 16:33:54 +0300

    Added A Mock Database

---
+++ RentingCars.Tests/Mocks/DatabaseMock.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentingCars.Data.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentingCars.Tests.Mocks
+{
+    public static class DatabaseMock
+    {
+        public static RentingCarsDbContext Instance 
+        {
+            get
+            {
+                var dbContextOptions = new DbContextOptionsBuilder<RentingCarsDbContext>()
+                    .UseInMemoryDatabase("RentingCarsInMemoryDb"
+                    + DateTime.Now.Ticks.ToString())
+                    .Options;
+
+                return new RentingCarsDbContext(dbContextOptions, false);
+            }
+        }
+    }
+}
