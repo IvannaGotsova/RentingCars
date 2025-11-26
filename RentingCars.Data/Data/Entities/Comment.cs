c77f9fe864a37423f95aa07575b15c2e7ee60cce
IvannaGotsova <ivannagotsova@outlook.com>
2025-11-26 10:48:38 +0200

    Created new entity - Comment

---
+++ RentingCars.Data/Data/Entities/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentingCars.Data.Data.Entities
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int CarId { get; set; }
+        public Car Car { get; set; }
+        public string UserName { get; set; }
+    }
+}
