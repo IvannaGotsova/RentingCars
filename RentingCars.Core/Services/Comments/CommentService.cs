8c4595e9099fbf8133a0af40c3c947eeee775145
IvannaGotsova <ivannagotsova@outlook.com>
2025-11-26 10:44:22 +0200

    Added new functionality - AddComment

---
+++ RentingCars.Core/Services/Comments/CommentService.cs
@@ -0,0 +1,38 @@
+﻿using RentingCars.Core.Services.ApplicationUsers;
+using RentingCars.Data.Data;
+using RentingCars.Data.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentingCars.Core.Services.Comments
+{
+    public class CommentService : ICommentService
+    {
+        private readonly RentingCarsDbContext rentingCarsDbContextdata;
+        private readonly IApplicationUserService applicationUserService;
+
+        public CommentService(RentingCarsDbContext rentingCarsDbContextdata, 
+            IApplicationUserService applicationUserService)
+        {
+            this.rentingCarsDbContextdata = rentingCarsDbContextdata;
+            this.applicationUserService = applicationUserService;
+        }
+
+        public void AddComment(int carId, string content, string userName)
+        {
+            var comment = new Comment
+            {
+                CarId = carId,
+                Content = content,
+                CreatedAt = DateTime.Now,
+                UserName = userName
+            };
+
+            rentingCarsDbContextdata.Comments.Add(comment);
+            rentingCarsDbContextdata.SaveChanges();
+        }
+    }
+}
---
+++ RentingCars.Core/Services/Comments/ICommentService.cs
@@ -0,0 +1,14 @@
+﻿using RentingCars.Data.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentingCars.Core.Services.Comments
+{
+    public interface ICommentService
+    {
+        void AddComment(int carId, string content, string userName);
+    }
+}
